d47307efccbecf736c23b02e6188d84c5740945a
Baizel Mathew <bmathew@financialforce.com>
2023-03-29 17:42:29 +0100

    refactor: output generator to include summary

--- src/results/OutputGenerator.ts
+++ src/results/OutputGenerator.ts
@@ -1,17 +1,25 @@
 /*
  * Copyright (c) 2019, FinancialForce.com, inc. All rights reserved.
  */
-import { ApexTestResult, OutcomeMap } from '../model/ApexTestResult';
+import {
+  ApexTestResult,
+  OutcomeMap,
+  CoverageReport,
+} from '../model/ApexTestResult';
 import { ApexTestRunResult } from '../model/ApexTestRunResult';
 import { Logger } from '../log/Logger';
 
+export interface TestRunSummary {
+  startTime: Date;
+  testResults: ApexTestResult[];
+  runResult: ApexTestRunResult;
+  coverageResult?: CoverageReport;
+}
 export interface OutputGenerator {
   generate(
     logger: Logger,
     outputFileBase: string,
-    startTime: Date,
-    tests: ApexTestResult[],
-    runResultSummary: ApexTestRunResult
+    summary: TestRunSummary
   ): void;
 }
 
