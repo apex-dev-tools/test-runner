9c71f7b5683c244bc2db343bac8196b7a8268c81
Peter Wright <pwright@financialforce.com>
2023-06-05 23:28:05 +0100

    test: update webpack config to resolve warnings

--- webpack.config.js
+++ webpack.config.js
@@ -21,5 +21,25 @@ const config = {
     mainFields: ['main'],
     extensions: ['.js'],
   },
+  ignoreWarnings: [
+    {
+      // ignore messages.readFile warning
+      // message refs are now transformed so the require won't be called
+      module: /@salesforce\/core\/lib\/messages.js/,
+    },
+  ],
+  plugins: [
+    // exclude html based cov reports to avoid adding css/image loaders
+    // uses the patched module to fix context detection
+    new webpack.ContextReplacementPlugin(
+      /istanbul-reports[/\\]lib$/,
+      /none|lcovonly|text|json/
+    ),
+    // ignore encoding import from node-fetch
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^encoding$/,
+      contextRegExp: /node-fetch[/\\]lib$/,
+    }),
+  ],
 };
 module.exports = config;
